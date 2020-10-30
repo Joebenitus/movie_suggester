1371d9a14ff1c0817fe0f7bdd20bd49068715650
Joseph Karnafel <josephkarnafel@gmail.com>
2020-10-30 08:56:20 -0700

    add ability to seed database with random movie titles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+  def self.begin
+    seed = Seed.new
+    seed.generate_movies
+  end
+
+  def generate_movies
+    25.times do |i|
+      movie = Movie.create!(title: Faker::Movie.title)
+      puts "Added movie: #{movie.title}"
+    end
+  end
+end
+
+Seed.begin
