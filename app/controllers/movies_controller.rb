6ad6622530311d6f11c85104f8a1f535b2838b3d
Joseph Karnafel <josephkarnafel@gmail.com>
2020-10-30 09:07:48 -0700

    add endpoints for full CRUD funtionality for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,35 @@
 class MoviesController < ApplicationController
   def index
-    @movies = {"movie": "Rise of the Planet of the Apes."}
+    @movies = Movie.all
     json_response(@movies)
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+    json_response(@movie)
+  end
+
+  def create
+    @movie = Movie.create!(movie_params)
+    json_response(@movie)
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
   end
 
   private
   def json_response(object, status = :ok)
     render json: object, status: status
+  end
+
+  def movie_params
+    params.permit(:title)
   end
 end
