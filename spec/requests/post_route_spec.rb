c3033fec84cb89c6dc3a43f40933823d4c5b1f2c
Joseph Karnafel <josephkarnafel@gmail.com>
2020-10-30 10:34:05 -0700

    add passing requdst spec for movie post method returning created status

--- spec/requests/post_route_spec.rb
+++ spec/requests/post_route_spec.rb
@@ -9,4 +9,8 @@ describe 'post a movie route', :type => :request do
   it 'returns the movie title' do
     expect(JSON.parse(response.body)['title']).to eq('test_title')
   end
+
+  it 'returns created status' do
+    expect(response).to have_http_status(:created)
+  end
 end
